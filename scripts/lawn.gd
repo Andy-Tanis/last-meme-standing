40cc04a26772458b382fbd8c9bd23a1a43b4e245
Andy-Tanis <atanis@brandner.com>
2024-05-29 18:55:28 -0700

    Add files via upload

--- scripts/lawn.gd
+++ scripts/lawn.gd
@@ -1,14 +1,43 @@
 extends Node2D
 
+var character = ["cloud", "karen"]
+
+var game_over := false
+
+var time_until_rotation := 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	
+	$ProgressBar.value = 50
+	
+	for n in $GridContainer.get_children():
+		var character_to_use = [0, 1].pick_random()
+		n.texture = load("res://assets/characters/" + character[character_to_use] + ".webp")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	
+	if time_until_rotation <= 0:
+		rotate_items()
+		time_until_rotation = 200
+	elif !game_over:
+		time_until_rotation -= 1
+	
+	if !game_over:
+		
+		if $ProgressBar.value < 100 and $ProgressBar.value > 0:
+			
+			$ProgressBar.value -= 0.05
+				
+		elif $ProgressBar.value == 100:
+			
+			win()
+			
+		elif $ProgressBar.value == 0:
+			
+			lose()
 
 
 func _on_button_mow_1_pressed() -> void:
@@ -24,6 +53,8 @@ func _on_button_mow_1_pressed() -> void:
 	tween.tween_callback(finished_mowing)
 	
 func lawnmower_spawned() -> void:
+	
+	time_until_rotation = 200
 	
 	$AudioLawnmower.play()
 	
@@ -57,8 +88,64 @@ func _on_button_mow_3_pressed() -> void:
 
 func finished_mowing() -> void:
 	
+	rotate_items()
+	
 	$Lawnmower.hide()
 	
-	$ButtonMow1.show()
-	$ButtonMow2.show()
-	$ButtonMow3.show()
+	if !game_over:
+		$ButtonMow1.show()
+		$ButtonMow2.show()
+		$ButtonMow3.show()
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("lawn_item"):
+		if area.get_parent().get_texture().get_path().contains("cloud"):
+			$Correct.play()
+			area.get_parent().get_node("Label").text = "+10"
+			$ProgressBar.value += 10
+		elif area.get_parent().get_texture().get_path().contains("karen"):
+			$Incorrect.play()
+			area.get_parent().get_node("Label").text = "-10"
+			$ProgressBar.value -= 10
+			
+		area.get_parent().get_node("Label").show()
+			
+		var tween = get_tree().create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+		tween.tween_property(area.get_parent(), "scale", Vector2(), 1)
+		tween.tween_property(area.get_parent(), "modulate", Color(1,1,1,0), .5)
+		tween.tween_callback(show_label.bind(area.get_parent().get_node("Label")))
+		tween.tween_property(area.get_parent(), "scale", Vector2(1, 1), .5)
+		tween.tween_property(area.get_parent(), "modulate", Color(1,1,1,1), 2)
+		
+		
+func show_label(label_node) -> void:
+	
+	label_node.hide()
+
+func rotate_items() -> void:
+	
+	for n in $GridContainer.get_children():
+		var character_to_use = [0, 1].pick_random()
+		var tween = get_tree().create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+		tween.tween_property(n, "modulate", Color(1,1,1,0), .1)
+		tween.tween_property(n, "modulate", Color(1,1,1,1), 1)
+		n.texture = load("res://assets/characters/" + character[character_to_use] + ".webp")
+		
+func win() -> void:
+	
+	game_over = true
+	$LabelYouWinLose.text = "You Won!"
+	$LabelYouWinLose/AnimationPlayer.play("show")
+	$Win.play()
+	$GridContainer.hide()
+	$ProgressBar.hide()
+	
+func lose() -> void:
+	
+	game_over = true
+	$LabelYouWinLose.text = "You Lost!"
+	$LabelYouWinLose/AnimationPlayer.play("show")
+	$Lose.play()
+	$GridContainer.hide()
+	$ProgressBar.hide()
